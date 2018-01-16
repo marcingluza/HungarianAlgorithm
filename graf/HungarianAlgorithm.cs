ce7f0df5f69aa541a412cf17c21e159352746822
Marcin Gluza <gumball300@gmail.com>
2018-01-16 23:28:31 +0100

    1.0

---
+++ graf/HungarianAlgorithm.cs
@@ -0,0 +1,227 @@
+﻿//////////////thx antifriz https://github.com/antifriz/hungarian-algorithm-n3/blob/master/src/HungarianAlgorithm.cs //////////////
+using System.Collections.Generic;
+
+namespace GraphAlgorithms
+{
+
+    public sealed class HungarianAlgorithm
+    {
+        private readonly int[,] _costMatrix;
+        private int _inf;
+        private int _n; //number of elements
+        private int[] _lx; //labels for workers
+        private int[] _ly; //labels for jobs 
+        private bool[] _s;
+        private bool[] _t;
+        private int[] _matchX; //vertex matched with x
+        private int[] _matchY; //vertex matched with y
+        private int _maxMatch;
+        private int[] _slack;
+        private int[] _slackx;
+        private int[] _prev; //memorizing paths
+
+        public HungarianAlgorithm(int[,] costMatrix)
+        {
+            _costMatrix = costMatrix;
+        }
+
+        public int[] Run()
+        {
+            _n = _costMatrix.GetLength(0);
+
+            _lx = new int[_n];
+            _ly = new int[_n];
+            _s = new bool[_n];
+            _t = new bool[_n];
+            _matchX = new int[_n];
+            _matchY = new int[_n];
+            _slack = new int[_n];
+            _slackx = new int[_n];
+            _prev = new int[_n];
+            _inf = int.MaxValue;
+
+
+            InitMatches();
+
+            if (_n != _costMatrix.GetLength(1))
+                return null;
+
+            InitLbls();
+
+            _maxMatch = 0;
+
+            InitialMatching();
+
+            var q = new Queue<int>();
+
+            #region augment
+
+            while (_maxMatch != _n)
+            {
+                q.Clear();
+
+                InitSt();
+
+                var root = 0;
+                int x;
+                var y = 0;
+
+                for (x = 0; x < _n; x++)
+                {
+                    if (_matchX[x] != -1) continue;
+                    q.Enqueue(x);
+                    root = x;
+                    _prev[x] = -2;
+
+                    _s[x] = true;
+                    break;
+                }
+
+                for (var i = 0; i < _n; i++)
+                {
+                    _slack[i] = _costMatrix[root, i] - _lx[root] - _ly[i];
+                    _slackx[i] = root;
+                }
+
+                while (true)
+                {
+                    while (q.Count != 0)
+                    {
+                        x = q.Dequeue();
+                        var lxx = _lx[x];
+                        for (y = 0; y < _n; y++)
+                        {
+                            if (_costMatrix[x, y] != lxx + _ly[y] || _t[y]) continue;
+                            if (_matchY[y] == -1) break;
+                            _t[y] = true;
+                            q.Enqueue(_matchY[y]);
+
+                            AddToTree(_matchY[y], x);
+                        }
+                        if (y < _n) break;
+                    }
+                    if (y < _n) break; 
+                    UpdateLabels(); 
+
+                    for (y = 0; y < _n; y++)
+                    {
+
+                        if (_t[y] || _slack[y] != 0) continue;
+                        if (_matchY[y] == -1)
+                        {
+                            x = _slackx[y];
+                            break;
+                        }
+                        _t[y] = true;
+                        if (_s[_matchY[y]]) continue;
+                        q.Enqueue(_matchY[y]);
+                        AddToTree(_matchY[y], _slackx[y]);
+                    }
+                    if (y < _n) break;
+                }
+
+                _maxMatch++;
+
+                int ty;
+                for (int cx = x, cy = y; cx != -2; cx = _prev[cx], cy = ty)
+                {
+                    ty = _matchX[cx];
+                    _matchY[cy] = cx;
+                    _matchX[cx] = cy;
+                }
+            }
+
+            #endregion
+
+            return _matchX;
+        }
+
+        private void InitMatches()
+        {
+            for (var i = 0; i < _n; i++)
+            {
+                _matchX[i] = -1;
+                _matchY[i] = -1;
+            }
+        }
+
+        private void InitSt()
+        {
+            for (var i = 0; i < _n; i++)
+            {
+                _s[i] = false;
+                _t[i] = false;
+            }
+        }
+
+        private void InitLbls()
+        {
+            for (var i = 0; i < _n; i++)
+            {
+                var minRow = _costMatrix[i, 0];
+                for (var j = 0; j < _n; j++)
+                {
+                    if (_costMatrix[i, j] < minRow) minRow = _costMatrix[i, j];
+                    if (minRow == 0) break;
+                }
+                _lx[i] = minRow;
+            }
+            for (var j = 0; j < _n; j++)
+            {
+                var minColumn = _costMatrix[0, j] - _lx[0];
+                for (var i = 0; i < _n; i++)
+                {
+                    if (_costMatrix[i, j] - _lx[i] < minColumn) minColumn = _costMatrix[i, j] - _lx[i];
+                    if (minColumn == 0) break;
+                }
+                _ly[j] = minColumn;
+            }
+        }
+
+        private void UpdateLabels()
+        {
+            var delta = _inf;
+            for (var i = 0; i < _n; i++)
+                if (!_t[i])
+                    if (delta > _slack[i])
+                        delta = _slack[i];
+            for (var i = 0; i < _n; i++)
+            {
+                if (_s[i])
+                    _lx[i] = _lx[i] + delta;
+                if (_t[i])
+                    _ly[i] = _ly[i] - delta;
+                else _slack[i] = _slack[i] - delta;
+            }
+        }
+
+        private void AddToTree(int x, int prevx)
+        {
+            _s[x] = true; 
+            _prev[x] = prevx;
+
+            var lxx = _lx[x];
+            for (var y = 0; y < _n; y++)
+            {
+                if (_costMatrix[x, y] - lxx - _ly[y] >= _slack[y]) continue;
+                _slack[y] = _costMatrix[x, y] - lxx - _ly[y];
+                _slackx[y] = x;
+            }
+        }
+
+        private void InitialMatching()
+        {
+            for (var x = 0; x < _n; x++)
+            {
+                for (var y = 0; y < _n; y++)
+                {
+                    if (_costMatrix[x, y] != _lx[x] + _ly[y] || _matchY[y] != -1) continue;
+                    _matchX[x] = y;
+                    _matchY[y] = x;
+                    _maxMatch++;
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ graf/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+using GraphAlgorithms;
+
+namespace graf
+{
+    public class Edge
+    {
+        string id;
+        string from;
+        string to;
+
+        public Edge(string _id, string _from, string _to)
+        {
+            this.id = _id;
+            this.from = _from;
+            this.to = _to;
+        }
+
+        public string Id { get => id; set => id = value; }
+        public string From { get => from; set => from = value; }
+        public string To { get => to; set => to = value; }
+    }
+    class Program
+    {
+
+        //String XmlString = @"P:\graf.xml";
+        
+        
+        static void Main(string[] args)
+        {
+            List<Edge> listOfEdges = new List<Edge>();
+            List<String> listOfNodes = new List<String>();
+
+            int nodesCount = 0;
+            int edgesCount = 0;
+
+            XmlDataDocument xmldoc = new XmlDataDocument();
+            XmlNodeList xmlnode_node, xmlnode_edge;
+            int i = 0;
+            FileStream fs = new FileStream(@"D:\\graf.xml", FileMode.Open, FileAccess.Read);
+            xmldoc.Load(fs);
+            xmlnode_node = xmldoc.GetElementsByTagName("node");
+            xmlnode_edge = xmldoc.GetElementsByTagName("edge");
+
+            for (i = 0; i <= xmlnode_node.Count - 1; i++)
+            {
+                string str_id = xmlnode_node[i].Attributes["id"].Value;
+                listOfNodes.Add(str_id);
+            }
+            nodesCount = xmlnode_node.Count - 1;
+
+            for (i = 0; i <= xmlnode_edge.Count - 1; i++)
+            {
+                string str_id = xmlnode_edge[i].Attributes["id"].Value;
+                string str_from = xmlnode_edge[i].Attributes["from"].Value;
+                string str_to = xmlnode_edge[i].Attributes["to"].Value;
+                Edge Temp = new Edge(str_id, str_from, str_to);
+                listOfEdges.Add(Temp);
+                
+            }
+            edgesCount = xmlnode_edge.Count - 1;
+
+            int[,] costMatrix = new int[nodesCount, nodesCount];
+
+            for(int k = 0; k < edgesCount; k++)
+            {
+
+                //UTWORZENIE MACIERZY KOSZTÓW
+              
+
+                    ///////////////////////TODO///////////////////////////////
+            }
+            int[,] array2D = new int[,] { { 2, 3, 5 }, { 5, 4, 3 }, { 3, 5, 4 } };
+            HungarianAlgorithm Algorytm = new HungarianAlgorithm(array2D);
+            Algorytm.Run();
+
+        }
+       
+
+    }
+}
